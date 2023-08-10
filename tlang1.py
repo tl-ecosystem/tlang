80bcc323c5bba8d3a75b399aa783f51735db5ed7
Theolaos TheooKing <nikosav2007@gmail.com>
2023-08-10 22:35:14 +0300

    partly support for variables and -log

--- tlang1.py
+++ tlang1.py
@@ -1,10 +1,10 @@
 import sys
 
-VER = 'v0.1'
+VER = 'v0.2'
 
-math_chars = ['+', '-', '/']
+math_chars = ['+', '-', '/', '*']
 logical_operations = ['>', '<', '!=', '==']
-escape_chars = [' ', ')'] + math_chars # TODO maybe delete later
+escape_chars = [' ', ')'] + math_chars + logical_operations # TODO maybe delete later
 printing_chars = ['(', ')']
 input_chars = ['(', ')']
 return_chars = ['(', ')']
@@ -28,17 +28,36 @@ def find_multiple(line, charset:list|str) -> dict: #TODO make this and then upda
         pos = [] #reseting pos of chars
     return(pos_dict)
 
-def print_method(line, char, vars):
+def print_method(line:str, vars):
     temp_var_list = []
     temp_line = list(line)
-
-    if line[line.find(char)+1] == '(' and line[len(line)-2] == ')':
-        for i in range(len(line[line.find(char)+2:len(line)-2])):
+    if line.startswith('p(') and line.endswith(')\n'):
+        for i in range(len(line[2:len(line)-2])):
             if line[i] == '$': #for variable handling
-                # temp_line.
-                # temp_var_list += vars[line.find('$')+1: ]
+                
                 pass
-        print(line[line.find(char)+2:len(line)-2])
+        print(line[2:len(line)-2])
+
+def find_loop(object:str, find) -> int:
+    num_of_finds = 0
+    start = 0
+    while object.find(find,start) != -1: 
+        num_of_finds += 1
+        start = object.find(find,start)
+        
+    return num_of_finds
+
+def logical_statement():
+    pass
+
+def bool_init(object):
+    object = object.replace(' ','')
+    if object == 'True':
+        return True
+    elif object == 'False':
+        return False
+    else:
+        raise Exception(f"{object} is not a bool")
 
 class InstructionSet():
     def __init__(self):
@@ -68,10 +87,41 @@ class Variables():
     def __init__(self):
         self.variables = {}
 
-    def variable_handler(self, line, char): #TODO
-        # if variable is to  
-        self.variables.update() #TODO FINISH THE variable handler
-                                #e.x. 'name' of variable
+    def variable_asssignment(self, line:str, LOGGING:bool=False): #TODO
+        var_value = None
+        try:
+            temp = line[line.find('=')+1: len(line)-1]
+        except:
+            print(f'Caught Syntax Error in line: {line}')
+
+        try:
+            var_value = int(temp)
+        except:
+            try:
+                var_value = float(temp)
+            except:
+                try:
+                    var_value = bool_init(temp)
+                except:
+                    var_value = str(temp)
+                
+
+        try:
+            self.variables.update({line[1:line.find('=')].replace(' ',''): var_value})
+            if LOGGING:
+                print(self.variables)
+        except Exception as error:
+            print(f'Problem with the naming of the variable: {type(error).__name__} \n{error}')
+            sys.exit()
+
+    def variable_handler(self): 
+        pass
+
+    def return_variable_value(self, name:str):
+        try:
+            return self.variables[name]
+        except:
+            print(f"The variable {name} hasn't been assigned.")
 
 class Interpreter(Variables, Functions):
     def __init__(self):
@@ -79,6 +129,7 @@ class Interpreter(Variables, Functions):
         Functions.__init__(self)
         self.argv = sys.argv
         self.ran = False # used if the interpreter completed what it was told to do
+        self.LOGGING = False #not as supposed to work, but near enough for now, also not exactly a constant
 
     def argv_handler(self):
         if self.argv[1] == '--help':
@@ -98,22 +149,35 @@ class Interpreter(Variables, Functions):
                   "Type --help to learn more\n")
             self.ran = True # used if the interpreter completed what it was told to do
 
-        elif self.argv[1] == '--docs': #TODO add the documentation here
+        elif self.argv[1] == '--docs': #TODO add the documentation here (p done)
             print("\nDocumentation with --docs\n"+
-                  "There is no documentation at the momment\n\n"+
+                  "Comments are anything that the interpreter doens't recognise as a special keyword/keychar\n\n"+
+                  "p() : can print stuff to the terminal\n"+
+                  "    e.x. p(hello world)\n\n"+
+                  "$name_var : you can create a variable with $ and then following it with the name \n"+
+                  "          For declaration you need to have it like so.\n"+
+                  "          e.x.    $var = hello world\n"+
+                  "          It will automatically assign the variable to it's type\n"+
+                  "          The types that the language supports are:\n"+
+                  "             1. Integer\n"+
+                  "             2. Float\n"+
+                  "             3. String\n"+
+                  "             4. Bool\n"+
+                  "There should always be \n"+
                   "Type --help to learn more\n")
             self.ran = True # used if the interpreter completed what it was told to do
     
     def script_execution_handler(self):
         if self.ran != True:
-            try:
-                with open(self.argv[1], 'r') as f:
-                    self.tscript = f.readlines()
-                self.script_execution()
-            except:
-                print(f'Exception Raised, type --help to learn more')
+            with open(self.argv[1], 'r') as f:
+                self.tscript = f.readlines()
+            if len(self.argv) > 2:
+                if self.argv[2] == '-log':
+                    self.LOGGING = True
+            self.script_execution()
 
-    def execute(self): #TODO change the execute function handles the argv, and make it more safe
+
+    def execute(self):
         if len(self.argv) > 1:
             self.argv_handler()
             self.script_execution_handler()
@@ -123,13 +187,16 @@ class Interpreter(Variables, Functions):
 
     def script_execution(self):
         for line in self.tscript: # line
-            for char in line: # char
-                if char == 'p': # print
-                    print_method(line, char, self.variables)
-                    break
-                if char == '$': # variable declaration
-                    self.variable_handler(line, char)
-                    break
+            if line.startswith('p(') != True and line.startswith('$') != True: # check if it is printing stuff
+                line = line.replace(' ','') #remove spaces
+
+            if line.startswith('p('): # print
+                print_method(line, self.variables)
+                continue
+            if line.startswith('$'): # variable declaration
+                self.variable_asssignment(line, self.LOGGING)
+                continue
+
 
 
 if __name__ == '__main__':
@@ -142,9 +209,10 @@ if __name__ == '__main__':
 #   tleng_inter.py --help [D]
 #   tleng_inter.py --version [D]
 #   tleng_inter.py --docs [/]
-# varibles
+# printing TODO change and add support to math, and separationg using , 
+# varibles TODO change string to be only gathered in ""
 # comments *
-# functions (Local variables, return)
+# functions (Local variables, return) later add support for kargs for functions
 # if statement
 # while/for loop
 
